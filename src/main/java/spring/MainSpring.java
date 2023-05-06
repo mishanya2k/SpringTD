5ce05a7ffceffaa76e8c60de923abacf76493ba9
mishanya2k <mihulk2000x@gmail.com>
2023-05-06 17:07:31 +1000

    without applicationContext.xml

--- src/main/java/spring/MainSpring.java
+++ src/main/java/spring/MainSpring.java
@@ -1,10 +1,16 @@
 package spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import spring.config.SpringConfig;
 
 public class MainSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        Playground playground = context.getBean("playground", Playground.class);
+//        playground.power();
+//        System.out.println(playground.play());
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         Playground playground = context.getBean("playground", Playground.class);
         playground.power();
         System.out.println(playground.play());
